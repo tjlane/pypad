4e504be6bb003b4918c1180c757983ed3134d16b
tjlane <thomas.joseph.lane@gmail.com>
2013-02-12 20:34:19 -0800

    cleaned up some stuff

--- assembler/assemble.py
+++ assembler/assemble.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 """
+A high-level interface to pyana for assembling images from psana geometry
+parameters. Main function to pay attention to is `assemble_image`.
 """
 
 import sys
@@ -20,21 +22,8 @@ import PyCSPadImage.GlobalGraphics     as gg # For test purpose in main only
 import PyCSPadImage.HDF5Methods        as hm # For test purpose in main only
 
 
-def get_event_from_hdf5(hd5_image_file, calibration_path, dsname, event):
-    
-    print 'Getting raw CSPad event %d from file %s \ndataset %s' % (event, fname, dsname)
-    ds1ev = hm.getAverageCSPadEvent( fname, dsname, event, nevents=5 )
-    if not ds1ev.shape == (32, 185, 388):
-        print 'WARNING: ds1ev.shape =', ds1ev.shape, "should be (32, 185, 388)"
-        
-    return ds1ev
-    
-    
-def get_event_from_npz(npz_image_file):
-    return np.load(npz_image_file)['arr_0']
-    
 
-def assemble_image(raw_image, calibration_path, run_number):
+def assemble_image(raw_image, calibration_path, run_number=0):
     """
     This function takes a 'raw' hd5 image file and returns an array representing
     the assembled image.
@@ -43,6 +32,15 @@ def assemble_image(raw_image, calibration_path, run_number):
     ----------
     raw_image : np.ndarray
         A shape=(32, 185, 388) array of the raw 1x1s
+    
+    calibration_path : str
+        The path to a directory containing all the calibration parameters,
+        which are in flat text files named runX-runY.data.
+        
+    run_number : int
+        The run number of the image. This indicates which parameters in
+        `calibration_path` should be used in the case that parameters changed
+        over the course of the runs. Defaults to run 0.
     
     Returns
     -------
@@ -60,6 +58,24 @@ def assemble_image(raw_image, calibration_path, run_number):
 
 
     return image
+
+
+def get_avg_from_hdf5(hd5_image_file, calibration_path, dsname, start_event,
+                      n_events_to_avg=5):
+    """
+    Extract an average image from a psana hdf5 file.    
+    """
+
+    print 'Getting raw CSPad event %d from file %s \ndataset %s' % (event, fname, dsname)
+    ds1ev = hm.getAverageCSPadEvent( fname, dsname, start_event, nevents=n_events_to_avg )
+    if not ds1ev.shape == (32, 185, 388):
+        print 'WARNING: ds1ev.shape =', ds1ev.shape, "should be (32, 185, 388)"
+
+    return ds1ev
+
+
+def get_event_from_npz(npz_image_file):
+    return np.load(npz_image_file)['arr_0']
     
     
 def plot_assembled_image(image):
@@ -76,7 +92,7 @@ def main():
     """
     
     # change the paths below to run this script
-    calibration_path = 'mikhail_geom'
+    calibration_path = 'example_calibration_dir' # Mikhail's hand optimized geom
     run_number = 58
     
     raw_image = get_event_from_npz('npz_examples/cxi64813_r58_evt1.npz')
