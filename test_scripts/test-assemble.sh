c01e67c54222424f1a299bfe6efd650728f7f363
tjlane <thomas.joseph.lane@gmail.com>
2013-02-28 23:31:03 -0800

    cleaned up mess

---
+++ test_scripts/test-assemble.sh
@@ -0,0 +1,1 @@
+optgeom data/test_images/gold/gold_avg.npz filter_params.yaml --param-dir data/ex_params/
---
+++ test_scripts/test-filter.sh
@@ -0,0 +1,1 @@
+calibrate-filter data/test_images/gold/gold_avg.npz --param-dir data/ex_params/
