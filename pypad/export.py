fixed masking wrong area bug

pypad/export.py
@@ -9,17 +9,12 @@ Library for exporting autogeom geometries to other software packages.
 import numpy as np
 import h5py
 
-def _check_geometry(geometry):
-    # if not (isinstance(geometry, CSPad) or isinstance(geometry, Metrology)):
-    #     raise TypeError('`geometry` must be either a CSPad or Metrology object')
-    return
-
     
 def to_cheetah(geometry, filename="pixelmap-cheetah-raw.h5"):
     """
     Convert a CSPad or Metrology object to a Cheetah h5 pixel map.
     
-    88Writes: 'pixelmap-cheetah-raw.h5'
+    Writes: 'pixelmap-cheetah-raw.h5'
     
     Parameters
     ----------
@@ -29,13 +24,12 @@ def to_cheetah(geometry, filename="pixelmap-cheetah-raw.h5"):
 		The file name for the output pixel map
     """
     
-    _check_geometry(geometry)
-    
     print "Exporting to Cheetah format..."
+    
     coordinates = ['x', 'y', 'z']
     
     pp = geometry.pixel_positions
-    assert pp.shape == (3, 4, 8, 185, 388)
+    assert pp.shape == (3, 4, 16, 185, 194)
     
     # write an h5
     f = h5py.File(filename, 'w')
@@ -53,7 +47,9 @@ def to_cheetah(geometry, filename="pixelmap-cheetah-raw.h5"):
                 y_start = 388 * j
                 y_stop  = 388 * (j+1)
                 quad = (j + 2) % 4 # cheetah's quads are not quite the same...
-                cheetah_image[x_start:x_stop,y_start:y_stop] = pp[x,quad,i,:,:]/1000 # jas: ensure cheetah geometry is in [m] and not [mm]
+                # jas: ensure cheetah geometry is in [m] and not [mm]
+                two_by_one = np.hstack(( pp[x,quad,i,:,:], pp[x,quad,i*2+1,:,:] )) / 1000.0
+                cheetah_image[x_start:x_stop,y_start:y_stop] = two_by_one
         
         f['/%s' % coordinates[x]] = cheetah_image
     

pypad/mask/padmask.py
@@ -18,7 +18,6 @@ class PadMask(object):
     An mask for a CSPad object.
     """
     
-    
     def __init__(self):
         """
         Initialize a CSPad mask object.
@@ -31,7 +30,7 @@ class PadMask(object):
     @property
     def mask(self):
         m = np.product( np.array(self._masks.values()), axis=0 )
-        assert m.shape == (4,8,185,388)
+        assert m.shape == (4,16,185,194)
         return m
         
         
@@ -92,7 +91,7 @@ class PadMask(object):
         """
         
         assert type(mask_name) == str
-        assert mask.shape == (4, 8, 185, 388)
+        assert mask.shape == (4, 16, 185, 194)
         
         if not mask.dtype == np.bool:
             mask = mask.astype(np.bool)
@@ -106,8 +105,8 @@ class PadMask(object):
         """
         Sanity check on `image`.
         """
-        if not image.shape == (4, 8, 185, 388):
-            raise ValueError('`image` must be shape (4, 8, 185, 388), got '
+        if not image.shape == (4, 16, 185, 194):
+            raise ValueError('`image` must be shape (4, 16, 185, 194), got '
                              '%s' % str(image.shape))
         return
         
@@ -116,7 +115,7 @@ class PadMask(object):
         """
         Utility function that just returns a blank mask.
         """
-        return np.ones((4, 8, 185, 388), dtype=np.int32)
+        return np.ones((4, 16, 185, 194), dtype=np.int32)
     
         
     # ----------
@@ -126,7 +125,7 @@ class PadMask(object):
     # to add a new kind of mask, make a new method here. Follow mask_threshold
     # as a template
     
-    def mask_pixel(self, quad, two_by_one, x, y):
+    def mask_pixel(self, quad, asic, x, y):
         """
         Mask a single pixel, or series of pixels. To do the latter, pass arrays
         as the arguments (even though the below says int).
@@ -136,20 +135,20 @@ class PadMask(object):
         quad : int
             [0,1,2,3], denoting the quad.
             
-        two_by_one : int
-            Int in [0,7], denoting 2x1.
+        asic : int
+            Int in [0,7], denoting asic.
             
         x : int
             Int in [0,184], denoting x position.
         
         y : int
-            Int in [0,388], denoting x position.
+            Int in [0,194], denoting x position.
         """
-        self._masks['base'][quad, two_by_one, x, y] = 0
+        self._masks['base'][quad, asic, x, y] = 0
         return
     
         
-    def unmask_pixel(self, quad, two_by_one, x, y):
+    def unmask_pixel(self, quad, asic, x, y):
         """
         Mask a single pixel, or series of pixels. To do the latter, pass arrays
         as the arguments (even though the below says int).
@@ -159,16 +158,16 @@ class PadMask(object):
         quad : int
             [0,1,2,3], denoting the quad.
             
-        two_by_one : int
-            Int in [0,7], denoting 2x1.
+        asic : int
+            Int in [0,7], denoting asic.
             
         x : int
             Int in [0,184], denoting x position.
         
         y : int
-            Int in [0,388], denoting x position.
+            Int in [0,194], denoting x position.
         """
-        self._masks['base'][quad, two_by_one, x, y] = 1
+        self._masks['base'][quad, asic, x, y] = 1
         return
     
         
@@ -225,7 +224,7 @@ class PadMask(object):
         m = self._blank_mask()
         
         for i in range(4):
-            for j in range(8):
+            for j in range(16):
                 for p in range(0, 185, 10):
                     
                     m[i,j,p,p] = 0
@@ -261,18 +260,18 @@ class PadMask(object):
             raise ValueError('`num_pixels` must be >0, <194')
         
         for i in range(4):
-            for j in range(8):
+            for j in range(16):
                 
                 # mask along the y-dim
                 m[i,j,:,0:n] = np.bool(False)
-                m[i,j,:,388-n:388] = np.bool(False)
+                m[i,j,:,194-n:194] = np.bool(False)
                 
                 # mask along the x-dim
                 m[i,j,0:n,:] = np.bool(False)
                 m[i,j,185-n:185,:] = np.bool(False)
                 
-                # mask a bar along y in the middle of the 2x1
-                m[i,j,:,194-n:194+n] = np.bool(False)
+                # # mask a bar along y in the middle of the 2x1
+                # m[i,j,:,194-n:194+n] = np.bool(False)
         
         self._inject_mask('border', m)
         
@@ -409,8 +408,8 @@ class MaskGUI(object):
         
         self.print_gui_help()
         
-        if not raw_image.shape == (4, 8, 185, 388):
-            raise ValueError("`raw_image` must have shape: (4, 8, 185, 388)")
+        if not raw_image.shape == (4, 16, 185, 194):
+            raise ValueError("`raw_image` must have shape: (4, 16, 185, 194)")
             
         if mask == None:
             self.mask = PadMask()
@@ -563,6 +562,12 @@ class MaskGUI(object):
     def on_keypress(self, event):
 
         if event.key in ['m', 'u']:
+            
+            if self.xy == None:
+                print "No area selected, mask not changed."
+            
+            # print "Masking region inside:"
+            # print self.xy
            
             # wrap around to close polygon
             self.xy = np.vstack(( self.xy, self.xy[0,:] ))
@@ -637,7 +642,7 @@ class MaskGUI(object):
         
     def _conv_2dinds_to_4d(self, inds):
         """
-        Convert indices in a Cheetah array to (4,8,185,388).
+        Convert indices in a 2d Cheetah array to (4,16,185,194).
         
         Parameters
         ----------
@@ -648,22 +653,23 @@ class MaskGUI(object):
         Returns
         -------
         inds_4d : np.ndarray, int
-            An N x 4 array, with each column indexing quads/2x1/x/y,
+            An N x 4 array, with each column indexing quads/asics/y/x,
         """
         
         inds_4d = np.zeros((inds.shape[0], 4), dtype=np.int32)
         
-        # abs 2x1 index = x / num_x + y / num_y * 2x1s-in-x
-        of32 = (inds[:,0] / 185) + (inds[:,1] / 388) * 8
-        assert np.all(of32 < 32)
+        # index each asic, in the correct order
+        # abs asic_index = x / num_x + y / num_y * asics-in-x
+        of64 = (inds[:,0] / 185) * 2 + (inds[:,1] / 388) * 16 + (inds[:,1] / 194) % 2
+        assert np.all(of64 < 64)
         
-        # quads / 2x1s
-        inds_4d[:,0] = of32 % 4
-        inds_4d[:,1] = of32 / 4
+        # quads / asics
+        inds_4d[:,0] = of64 / 16
+        inds_4d[:,1] = of64 % 16
         
         # x / y
         inds_4d[:,2] = inds[:,0] % 185
-        inds_4d[:,3] = inds[:,1] % 388
+        inds_4d[:,3] = inds[:,1] % 194
         
         return inds_4d
         
@@ -702,7 +708,7 @@ class MaskGUI(object):
         print "             borders at least."
         print ""
         print "                        ----- // -----"
-        
+        print
         
         
         

pypad/utils.py
@@ -190,8 +190,8 @@ def radial_profile(image, center):
 
 def flatten_2x1s(image):
     """
-    Takes a non-2D image : either (32, 185, 388) or (4, 8, 185, 388) and returns
-    a 2d version of that image (for visualization).
+    Takes a non-2D image : (32, 185, 388), (4, 8, 185, 388), (4, 16, 185, 194) 
+    and returns a 2d version of that image (for visualization).
     """
     
     flat_image = np.zeros((185*8, 388*4))
@@ -213,6 +213,16 @@ def flatten_2x1s(image):
                 ys = 388 * i
                 ye = 388 * (i + 1)
                 flat_image[xs:xe,ys:ye] = image[i,j,:,:] 
+                
+                
+    elif image.shape == (4, 16, 185, 194):
+        for i in range(4):
+            for j in range(8):
+                xs = 185 * j
+                xe = 185 * (j + 1)
+                ys = 388 * i
+                ye = 388 * (i + 1)
+                flat_image[xs:xe,ys:ye] = np.hstack((image[i,j*2,:,:], image[i,j*2+1,:,:]))
         
     else:
         raise ValueError('Invalid shape for arg `image`')