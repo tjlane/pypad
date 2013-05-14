12be65399d3fc851d84ff1dce701516ddb55a2d1
tjlane <thomas.joseph.lane@gmail.com>
2013-05-14 15:50:23 -0700

    added QC metrics and upgraded metrology parsing

--- pypad/cspad.py
+++ pypad/cspad.py
@@ -413,6 +413,7 @@ class CSPad(object):
         
         self.metrology_basis = [[],[],[],[]]
         for q in range(4):
+            print "\nParsing: quad %d" % q
             for two_by_one in range(8):
                 self.metrology_basis[q].append( self._twobyone_to_bg(qms[q], two_by_one) )
         
@@ -502,7 +503,7 @@ class CSPad(object):
         return quad_metrologies
 
 
-    def _twobyone_to_bg(self, quad_metrology, two_by_one_index):
+    def _twobyone_to_bg(self, quad_metrology, two_by_one_index, tol=1e-5):
         """
         Convert a 2x1 in the optical metrology into two basis grid elements.
 
@@ -514,6 +515,15 @@ class CSPad(object):
         two_by_one_index : int
             The index of the 2x1 to check out.
             
+        Optional Parameters
+        -------------------
+        tol : float
+            The tolerance for a quality control assessment that is performed on
+            the optical measurement, including checking things that should be
+            orthogonal are, distances are correct, etc. The float `tol` can be
+            interpreted as a tolerane for error for each of these in units of
+            mm.
+            
         Returns
         -------
         bgs : tuple
@@ -522,6 +532,19 @@ class CSPad(object):
 
         if not quad_metrology.shape == (32,3):
             raise ValueError('Invalid quad_metrology, must be shape (32,3), got: %s' % str(quad_metrology.shape))
+
+
+        def qc_check(value, err_msg=None):
+            """
+            Simple helper function for performing quality control checks.
+            """
+            if not np.abs(value) <= tol:
+                print "WARNING: Metrology quality control failed for 2x1: %d" % two_by_one_index
+                if err_msg:
+                    print '--> ' + err_msg
+                print "    Val: %f // tol: %f" % (value, tol)
+            return
+        
 
         # below is the sequence in which each 2x1 is optically measured, and
         # thus this sequence is read out of the metrology
@@ -543,9 +566,9 @@ class CSPad(object):
         # 0.10992 mm : pixel size
         # 0.27480 mm : gap between ASICS in a 2x1
         
-        fl = 2* 194 * 0.10992 + 0.27480  # length of the long side of a 2x1
-        sl = 185 * 0.10992               # length of the short side of a 2x1
-        h  = np.sqrt( fl*fl + sl*sl )    # length of hypothenuse of an ASIC
+        fl = 2 * 194 * 0.10992 + 0.27480  # length of the long side of a 2x1
+        sl = 185 * 0.10992                # length of the short side of a 2x1
+        h  = np.sqrt( fl*fl + sl*sl )     # length of hypothenuse of an ASIC
 
         # Next, we build a basis grid representation of each 2x1, which consists
         # of two individual ASICS. Each ASIC has an "origin" which denotes the
@@ -554,51 +577,94 @@ class CSPad(object):
         # grid of pixels along the slow and fast scan directions (here, short
         # and long sizes) of the 2x1 respectively.
         
+        # The s/f vectors are computed by averaging the vectors that define each
+        # side in the optical measurement.
+        
+        # The p vector is measured based on placing a point the correct distance
+        # along the diagonal between two measured points. This is checked for
+        # correctness by ensuring that the diagonal used is orthogonal to the
+        # other 2x1 diagonal measured
+        
         # NOTE : later, we swap x to reach CXI convention -- this is done after
         #        we position the quads in _generate_basis()
         
         if two_by_one_index in [0,1]:
 
-            s = 0.10992 * self._unit(xyz_2x1[0,:] - xyz_2x1[1,:])
-            f = 0.10992 * self._unit(xyz_2x1[2,:] - xyz_2x1[1,:])
+            s = 0.10992 * self._unit( (xyz_2x1[0,:] - xyz_2x1[1,:]) +
+                                      (xyz_2x1[3,:] - xyz_2x1[2,:]) )
+            f = 0.10992 * self._unit( (xyz_2x1[2,:] - xyz_2x1[1,:]) +
+                                      (xyz_2x1[3,:] - xyz_2x1[0,:]) )
             
             diagonal = (xyz_2x1[3,:] - xyz_2x1[1,:]) / 1000.
+            center = np.sum(xyz_2x1, axis=0) / 1000.
             offset = (np.linalg.norm(diagonal) - h) / 2.
             
-            p0 = xyz_2x1[1,:] / 1000. + offset * self._unit(diagonal)
+            p0 = xyz_2x1[1,:] / 1000. + offset * self._unit(center)
             p1 = p0 + shape[1] * f + 0.27480 * self._unit(f) # for 3px gap
 
 
         elif two_by_one_index in [2,3,6,7]:
 
-            s = 0.10992 * self._unit(xyz_2x1[1,:] - xyz_2x1[2,:])
-            f = 0.10992 * self._unit(xyz_2x1[3,:] - xyz_2x1[2,:])
-
+            s = 0.10992 * self._unit( (xyz_2x1[1,:] - xyz_2x1[2,:]) +
+                                      (xyz_2x1[0,:] - xyz_2x1[3,:]) )
+            f = 0.10992 * self._unit( (xyz_2x1[3,:] - xyz_2x1[2,:]) + 
+                                      (xyz_2x1[0,:] - xyz_2x1[1,:]) )
+                                      
             diagonal = (xyz_2x1[0,:] - xyz_2x1[2,:]) / 1000.
+            center = np.sum(xyz_2x1, axis=0) / 1000.
             offset = (np.linalg.norm(diagonal) - h) / 2.
 
-            p0 = xyz_2x1[2,:] / 1000. +  offset * self._unit(diagonal)
+            p0 = xyz_2x1[2,:] / 1000. +  offset * self._unit(center)
             p1 = p0 + shape[1] * f + 0.27480 * self._unit(f) # for 3px gap
 
 
         elif two_by_one_index in [4,5]:
 
-            s = 0.10992 * self._unit(xyz_2x1[2,:] - xyz_2x1[3,:])
-            f = 0.10992 * self._unit(xyz_2x1[0,:] - xyz_2x1[3,:])
+            s = 0.10992 * self._unit( (xyz_2x1[2,:] - xyz_2x1[3,:]) +
+                                      (xyz_2x1[1,:] - xyz_2x1[0,:]) )
+            f = 0.10992 * self._unit( (xyz_2x1[0,:] - xyz_2x1[3,:]) +
+                                      (xyz_2x1[1,:] - xyz_2x1[2,:]) )
 
             diagonal = (xyz_2x1[1,:] - xyz_2x1[3,:]) / 1000.
+            center = np.sum(xyz_2x1, axis=0) / 1000.
             offset = (np.linalg.norm(diagonal) - h) / 2.
 
-            p0 = xyz_2x1[3,:] / 1000. +  offset * self._unit(diagonal)
+            p0 = xyz_2x1[3,:] / 1000. +  offset * self._unit(center)
             p1 = p0 + shape[1] * f + 0.27480 * self._unit(f) # for 3px gap
 
 
         else:
             raise ValueError('two_by_one_index must be in 0...7')
+            
+            
+        # --- perform some quality control checks ---
+        
+        # (1) ensure s/f orthogonal
+        qc_check(np.dot(s, f), err_msg='s/f vectors are not orthogonal')
+        
+        # --- end QC ---------------------------------
+        
+        
+        # no matter what, correct the s/f vectors so they are orthogonal
+        axis = np.cross(s, f)
+
+        # the angle between s/f
+        theta = np.arccos(np.dot(s, f) / ( np.linalg.norm(s) * np.linalg.norm(f) ))
+        rot = ((np.pi / 2.0) - theta) / 2.0
+        
+        # rotate each vector -- below `rot` is deg ccw wrt x-axis in the ref
+        # frame of '+axis'
+        Rs = utils.ER_rotation_matrix(-axis,  rot)
+        Rf = utils.ER_rotation_matrix(-axis, -rot)
+        
+        s = np.dot(Rs, s)
+        f = np.dot(Rs, f)
         
         # return a tuple of basis grid objects
         bgs = ( (p0.copy(), s.copy(), f.copy(), shape), 
                 (p1.copy(), s.copy(), f.copy(), shape) )
+                
+        
         return bgs
     
         
