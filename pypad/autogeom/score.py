updated the peak matching algorithm in score so that it finds the proper peaks even after the angular sum was changed to angular average. also fixed a bug for plotting the calibration images in score, 'n' and 'l' now loops around instead of going out of bounds.

pypad/autogeom/score.py
@@ -245,36 +245,119 @@ class PowderReference(object):
     def _match_peaks(self, sample_index):
         """
         Automatically match observed powder rings to miller indices. Currently
-        will match observed peaks to the closest expected peak.
-        """
+        will match observed peaks to the closest expected peak weighted by its
+        peak intensity.
         
-        # (1) match observed peaks to their closest expected peak
-        # (2) for expected peaks with more than one match, keep only the tallest
-        #     peak
+        Returns
+        --------
+        m_observed: np.ndarray
+            A one-d array containing the q-positions of the observed peaks that
+            matches the expected peaks the best
+        """
         
         observed = self.reciprocal(self.sample_peak_locs[sample_index], \
                                self.sample_distances[sample_index] + \
                                    self.distance_offset )
         expected = self.expected
         
-        observed_matches = np.zeros( len(observed), dtype=np.int32 )
-        m_observed = np.zeros_like(expected)
+        ### Old algorithm by TJ ###
+        # (1) match observed peaks to their closest expected peak
+        # (2) for expected peaks with more than one match, keep only the tallest
+        #     peak
         
-        # match each observed value with its closest expected value
-        for i in range(len(observed)):
-            match_index     = np.argmin( np.abs(expected - observed[i]) )
-            observed_matches[i] = match_index
-            
-        # for each expected value, if has multi matches, choose best
-        for i in range(self.num_millers):
-            w = (observed_matches == i)
-            if np.sum(w) > 1:
-                mx = np.argmax( self.sample_peak_heights[sample_index][w] )
-                m_observed[i] = observed[ np.where(observed_matches == i)[0][mx] ]
+#        observed_matches = np.zeros( len(observed), dtype=np.int32 )
+#        m_observed = np.zeros_like(expected)
+#        
+#        # match each observed value with its closest expected value
+#        for i in range(len(observed)):
+#            match_index     = np.argmin( np.abs(expected - observed[i]) )
+#            observed_matches[i] = match_index
+#        
+#        # for each expected value, if has multi matches, choose best
+#        for i in range(self.num_millers):
+#            w = (observed_matches == i)
+#            if np.sum(w) > 1:
+#                mx = np.argmax( self.sample_peak_heights[sample_index][w] )
+#                m_observed[i] = observed[ np.where(observed_matches == i)[0][mx] ]
+        
+        
+        ### New algorithm by Jonas ###
+        # Will optimize the observed peak indices that corresponds to the expected peaks
+        # based on the proximity of the peak position in q weighted by its peak intensity.
+        # This algorithm assumes:
+        # (1) all expected peaks exist in observed peaks
+        #     (i.e. has to be found by the peak finder in _load_calibration_sample()
+        #      and be present on the detector at all detector distances)
+        # (2) the peaks in observed and expected are ordered after increasing q
+        
+        observed_matches = np.zeros( self.num_millers, dtype=np.int32 )
+        assert len(expected) == self.num_millers
+        
+        opt = 1e30
+        for i in utils.multi_for(map( xrange, np.arange(self.num_millers), np.ones(self.num_millers, dtype=np.int32 )*len(observed) - np.arange(self.num_millers)[::-1] )):
+            # only try peak index combinations where all indices are different
+            if len(i) == len(set(i)):
+                indices = [x for x in i] # can't access indices with tuple, need to make it into a list
+                new_opt = sum(((expected-observed[indices])/self.sample_peak_heights[sample_index][indices])**2)
+                if new_opt < opt:
+                    opt = new_opt
+                    observed_matches[:] = indices
+        
+        m_observed = observed[observed_matches]
+        
+        ### OPTION #2 by Jonas ###
+        # currently doesn't work since the optimization algorithm fails for discrete variables,
+        # could be worth trying if a good linear optimizer for discrete variables is found. This could be a candidate:
+        # http://stackoverflow.com/questions/5179889/optimization-problem-in-python
+        
+#        def peak_objective(args):
+#            """
+#            The peak objective function -- the difference between the measured/
+#            expected peak locations weighted by their intensity.
+#            """
+#            
+#            # make sure that indices are integer numbers
+#            for i in range(len(args)):
+#                if args[i] >= (len(observed) - 1):
+#                    args[i] = len(observed) - 1
+#                elif args[i] < 0:
+#                    args[i] = 0
+#                else:
+#                    args[i] = round(i)
+#            args = np.array( args, dtype=np.int32 ).flatten()
+#            
+#            obj = (expected - observed[args])/self.sample_peak_heights[sample_index][args]
+#            
+#            return obj.flatten()
+#        
+#        i0 = np.arange(self.num_millers)
+#        #opt = optimize.leastsq(peak_objective, i0, args=(expected, observed), full_output=1)
+#        opt = optimize.leastsq(peak_objective, i0, full_output=1)
+#        
+#        observed_matches = np.array( opt[0], dtype=np.int32 )
+#        m_observed = observed[observed_matches]
+        
+        ### OPTION #3 by Jonas ###
+        # A similar algorithm to the one currently used, except that it reduces the
+        # number of loops by adjusting the lower range of the inner for loop depending
+        # on the outer loop index. Unfortunately it is hard to generalize for arbitrary
+        # number of Bragg peaks (i.e. self.num_millers)
+        
+#        observed_matches = np.zeros( self.num_millers, dtype=np.int32 )
+#        assert len(expected) == self.num_millers
+#        
+#        opt = 1e30
+#        for i in range(len(observed) - 1):
+#            for j in range(i + 1, len(observed)):
+#                indices = [i, j]
+#                new_opt = sum(((expected-observed[indices])/self.sample_peak_heights[sample_index][indices])**2)
+#                if new_opt < opt:
+#                    opt = new_opt
+#                    observed_matches[:] = indices
         
         return m_observed
     
-        
+    
     def evaluate(self):
         """
         The central function -- evaluates the geometry by fitting the energy,
@@ -343,7 +426,7 @@ class PowderReference(object):
         self._axL.cla()
         self._axR.cla()
         
-        if (index < 0) or (index > self.num_samples):
+        if (index < 0) or (index >= self.num_samples):
             print "Cannot access sample: %d" % index
             print "Total %d samples available" %  self.num_samples
             return
@@ -395,10 +478,14 @@ class PowderReference(object):
     
     def _on_keypress(self, event):
         if event.key == 'l':
+            if (self._current_image % self.num_samples) == 0:
+                self._current_image += self.num_samples
             self._current_image -= 1
             self._plot_cal_sample(self._current_image)
         elif event.key == 'n':
             self._current_image += 1
+            if (self._current_image % self.num_samples) == 0:
+                self._current_image -= self.num_samples
             self._plot_cal_sample(self._current_image)
         else:
             return