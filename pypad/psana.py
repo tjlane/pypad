0bff5eefd6dc3c16391a50aca72150b90b37ba1f
TJ Lane <tjlane@stanford.edu>
2014-04-01 12:12:58 -0700

    added polar interp conv fxn

--- pypad/psana.py
+++ pypad/psana.py
@@ -8,7 +8,7 @@ import os
 import numpy as np
 
 import cspad
-
+from read import enforce_raw_img_shape
 
 
 def get_geometry(experiment, run, camera, energy, distance, dilation,
@@ -55,4 +55,49 @@ def get_geometry(experiment, run, camera, energy, distance, dilation,
     #print camera, corr_dist, delta_dilt, energy, dtc.q_max
 
     return dtc, geom, mask
+
+
+def interp_to_polar(raw_image, q_values, dtc, num_phi=1024, mask=None):
+    """
+    Interpolate a single image into polar coordiantes. Requires thor.
+
+    Parameters
+    ----------
+    raw_image : np.ndarray
+        The raw pixel intensities, in shape (4, 16, 185, 194)
+
+    q_values : np.ndarray
+        A 1d list of the q-values to interpolate into
+
+    dtc : thor.Detector
+        A detector object
+
+    Optional Parameters
+    -------------------
+    num_phi : int
+        The number of phi slices to interpolate (more = higher res)
+
+    mask : np.ndarray
+        A pixel mask to apply
+
+    Notes
+    -----
+    Not elegant code, but it works
+    """
+    try:
+        import thor
+    except ImportError as e:
+        print e
+        raise ImportError('Thor must be installed to employ the interp_to_polar'
+                          'function...')
+
+    if mask == None:
+        mask = np.ones((4, 16, 185, 194))
+
+    thor_format = enforce_raw_img_shape(raw_image).flatten()
+    ss = thor.Shotset(thor_format, dtc, mask=mask.flatten())
+    r = ss.to_rings(q_values, num_phi=num_phi)
+
+    return r.polar_intensities[0] * r.polar_mask
+
 
